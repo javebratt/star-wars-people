4724bc4c4442d31e096ad77b8ed7802676eec426
Jorge Vergara <jorge@jorgevergara.co>
2023-07-02 20:07:20 -0400

    chore: renames app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'start-wars-people',
+  appName: 'star-wars-people',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
